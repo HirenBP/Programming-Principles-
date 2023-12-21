4fc47f350d0d590589c2b58f7e2a19c94aad591f
Hiren <hiren.bhavsar@griffithuni.edu.au>
2023-12-21 17:51:49 +1100

    Workshop 7.

--- QuickTest2.py
+++ QuickTest2.py
@@ -1,4 +1,11 @@
-import re
-s1 = input('String:')
-match = re.findall(r'gg', s1)
-print('IsHappy?:', len(match) > 0)
+import os
+
+print(os.getcwd())
+path = os.getcwd()
+print(os.path.dirname(path))
+print(os.path.basename(path))
+print(os.path.split(path))
+print(os.path.splitext(path))
+print(os.path.join(os.path.dirname(path), os.path.basename(path)))
+print(os.path.exists(path))
+print(os.path.isfile(path))
---
+++ WS7/Problem1.py
@@ -0,0 +1,21 @@
+"""
+Problem: Write a program that prompts for the names of a source file to read and a target file to write, and copy the
+content of the source file to the target file, but with all empty lines removed, then output the number of
+empty lines removed.
+"""
+import os
+# source = "Data_Files" + os.sep + input("Enter the name of the source file: ")
+source = "C:\\Users\hiren\PycharmProjects\Programming-Principles-\WS7\Data_Files\\file.txt"
+target = "Data_Files" + os.sep + input("Enter the name of the target file: ")
+
+with open(source, "r") as f:
+    with open(target, 'w') as f1:
+        lines = f.readlines()
+        empty_lines = 0
+        # Iterate over the lines
+        for line in lines:
+            if line == "\n":
+                empty_lines += 1
+            else:
+                f1.write(line)
+        print(f'Number of lines remoed {empty_lines}')
